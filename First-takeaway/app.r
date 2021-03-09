bc826003a690ca61494163d20627c29b644671d0
Daniel A <dreth+services@protonmail.com>
2021-03-09 16:51:24 +0100

    adding plots function, 2nd tab page

--- First-takeaway/app.r
+++ First-takeaway/app.r
@@ -2,18 +2,68 @@ require(shiny)
 require(tidyverse)
 require(shinyjs)
 require(shinythemes)
+require(dplyr)
+require(ggplot2)
+require(stringr)
+require(PerformanceAnalytics)
+require(foreach)
+require(MASS)
+require(gridExtra)
 
 dem <- read.csv('./data/data.csv')
+
+# Function used to generate plots on the tab "histograms and boxplots"
+# function to plot
+plots <- function(dataset, col, fw=FALSE, hist='default',
+                  density='default' , bins='default',
+                  xtick_angles='default', sep=FALSE, savefig='default', filename='./plot.png') {
+    var <- dataset %>% dplyr::select(col)
+    if (bins == 'default') {bins <- rep(10,2)}
+    if (xtick_angles == 'default') {xtick_angles <- rep(90,2)}
+    if (hist == 'default') {hist <- c(FALSE,FALSE)}
+    if (density == 'default') {density <- c(TRUE,TRUE)}
+    if (savefig == 'default') {savefig <- c(FALSE,14,14)}
+
+    p1 <- dataset %>% ggplot(aes(x=var[,1])) +
+        geom_boxplot() +
+        ggtitle(str_interp("${col}")) +
+        theme(axis.title.x=element_blank(),axis.text.y=element_blank())
+    p2 <- dataset %>% ggplot(aes(x=var[,1], fill=hdi_cat)) +
+        geom_boxplot() +
+        ggtitle(str_interp("${col} grouped by HDI")) +
+        theme(axis.title.x=element_blank(),axis.text.y=element_blank())
+    p3 <- dataset %>% ggplot(aes(x=var[,1])) +
+        ggtitle(str_interp("${col}")) +
+        theme(axis.title.x=element_blank(),
+                axis.text.x = element_text(angle = xtick_angles[1]))
+    p4 <- dataset %>% ggplot(aes(x=var[,1])) +
+        ggtitle(str_interp("${col} by HDI group")) +
+        theme(axis.title.x=element_blank(),
+                axis.text.x = element_text(angle = xtick_angles[2]))
+    if (hist[1] == TRUE) {
+        p3 <- p3 + geom_histogram(aes(y=..density..),bins=bins[1])}
+    if (hist[2] == TRUE) {
+        p4 <- p4 + geom_histogram(show.legend = FALSE,bins=bins[2],
+                                  aes(fill=hdi_cat,y=..density..))}
+    if (density[1] == TRUE) {
+        p3 <- p3 + geom_density()}
+    if (density[2] == TRUE) {
+        p4 <- p4 + geom_density(aes(group=hdi_cat,colour=hdi_cat,fill=hdi_cat))}
+    if (fw == TRUE) {p4 <- p4 + facet_wrap(~hdi_cat, nrow = 1)}
+    if (sep == TRUE) {
+        grid.arrange(p1,p2, nrow=2)
+        grid.arrange(p3,p4, nrow=2)}
+    else {grid.arrange(p1,p2,p3,p4, nrow=4)}
+    if (savefig[1] == TRUE) {ggsave(file=filename, width=savefig[2], height=savefig[3],
+                                 arrangeGrob(p1,p2,p3,p4, nrow=4))}
+}
+
 
 shinyApp(
     # UI of the application
     ui = navbarPage(theme = shinytheme("slate"), "Country metrics",
             # Introduction page
             tabPanel("Introduction",
-                # Custom CSS
-                tags$head(
-                    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
-                ),
                 fluidPage(id="introductionPage",
                     h1("Country metrics"),
                     h3("A set of visualizations for different worldwide demographic/development metrics"),
@@ -29,11 +79,23 @@ shinyApp(
                 
             ),
             # 
-            tabPanel("",
-            
+            tabPanel("Histograms/Boxplots",
+                sidebarLayout(
+                    sidebarPanel(
+                        sidebarPanel(
+                            selectInput("selectVarHB",
+                                label = h4("Select variable to plot"),
+                                choices = names(dem)[3:length(names(dem)-1)],
+                                selected = 1),
+                            
+                    ),
+                    mainPanel(
+                        plotOutput(outputId = "multiPlotsBasic")
+                    ),
+                ),
             )
         ),
-
+    
     # Server function for the shiny app
     server = function(input, output) {
         
