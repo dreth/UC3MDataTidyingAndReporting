5010aab3352e1a1cd3028feb07801500a1ebb350
Daniel A <dreth+services@protonmail.com>
2021-03-09 16:01:41 +0100

    introduction and cleaning file tree

--- First-takeaway/app.r
+++ First-takeaway/app.r
@@ -1,17 +1,28 @@
 require(shiny)
 require(tidyverse)
 require(shinyjs)
+require(shinythemes)
 
-dem <- read.csv('./data.csv')
+dem <- read.csv('./data/data.csv')
 
-# Define UI for app that draws a histogram ----
-ui <- navbarPage("Country metrics",
-        tabPanel("",
+ui <- navbarPage(theme = shinytheme("slate"), "Country metrics",
+        tabPanel("Introduction",
+            fluidPage(
+                h1("Country metrics"),
+                h3("A set of visualizations for different worldwide demographic/development metrics"),
+                p("Obtained from the",
+                    a("World bank Databank",href="https://databank.worldbank.org/home.aspx"),
+                    "specifically the",
+                    a("World Development Indicators database",href="https://databank.worldbank.org/source/world-development-indicators"),
+                    "This is the 'primary World Bank collection of development indicators' as stated on the database description. It has lots of economic, education, energy use, and population specific metrics."),
+                p("The dataset used (as it was used) can be found in the file tree of the repository for this shiny app. the file is easily identifiable within a folder called 'data' and named 'data.csv'."),
+                br(),
+                img(src="worldbanklogo.png", width=711.11, height=400)
+            ),
             
-        ),
+        )
     )
 
-# Define server logic required to draw a histogram ----
 server <- function(input, output) {
     
 }
